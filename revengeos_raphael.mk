c58596d7d470bfa2aad0d4805e697da085f80be9
nayspl <hass.przybylo@gmail.com>
2020-07-09 12:40:49 +0200

    raphael: fix revengeos stuff

--- revengeos_raphael.mk
+++ revengeos_raphael.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common Revenge stuff.
-$(call inherit-product, vendor/revengeos/config/common_full_phone.mk)
+$(call inherit-product, vendor/revengeos/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1440
 
 # Device identifier. This must come after all inclusions.
